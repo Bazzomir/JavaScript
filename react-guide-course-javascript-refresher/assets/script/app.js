c031de57f652c2e982938fb70e4b675d8a994592
bazzomir <bazzomir97@gmail.com>
2025-03-14 04:56:10 +0100

    lesson 3

--- react-guide-course-javascript-refresher/assets/script/app.js
+++ react-guide-course-javascript-refresher/assets/script/app.js
@@ -14,29 +14,46 @@
 // console.log(until);
 // console.log(greating);
 
-const user = {
-    name: "Max",
-    age: 34,
-    greet() {
-        console.log("Hello!");
-        console.log(this.age);
-    },
-};
+// const user = {
+//     name: "Max",
+//     age: 34,
+//     greet() {
+//         console.log("Hello!");
+//         console.log(this.age);
+//     },
+// };
 
-console.log(user.name);
-user.greet();
+// console.log(user.name);
+// user.greet();
 
-class User {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
+// class User {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
 
-    greet() {
-        console.log("Hi!");
-    }
-}
+//     greet() {
+//         console.log("Hi!");
+//     }
+// }
 
-const user1 = new User("Manuel", 34);
-console.log(user1);
-user1.greet();
+// const user1 = new User("Manuel", 34);
+// console.log(user1);
+// user1.greet();
+
+
+const hobbies = ["Sports", "Cooking", "Reading"];
+console.log(hobbies[0]);
+
+hobbies.push("Working");
+console.log(hobbies);
+
+// const index = hobbies.findIndex((item) => {
+//   return item === "Sports";
+// });
+const index = hobbies.findIndex((item) => item === "Sports"); //isto kako pogore, samo najkratka verzija
+console.log(index);
+
+const editedHobbies = hobbies.map((item) => item + "!");
+const editedHobbies2 = hobbies.map((item) => ({ text: item }));
+console.log(editedHobbies, editedHobbies2);
