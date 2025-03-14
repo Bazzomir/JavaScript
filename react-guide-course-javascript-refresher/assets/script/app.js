a431660cb112d6fa94ef77c07a6b1f236313d67b
bazzomir <bazzomir97@gmail.com>
2025-03-14 06:03:19 +0100

    lesson 4

--- react-guide-course-javascript-refresher/assets/script/app.js
+++ react-guide-course-javascript-refresher/assets/script/app.js
@@ -1,3 +1,4 @@
+// lesson 1
 // import { export1, export2 as content2 } from "./util";
 // import export3 from "./util";
 // import * as until from "./util"; // kreira object od se
@@ -13,7 +14,9 @@
 // console.log(export1 + " and " + content2 + "..." + export3);
 // console.log(until);
 // console.log(greating);
+//
 
+// lesson 2
 // const user = {
 //     name: "Max",
 //     age: 34,
@@ -40,20 +43,40 @@
 // const user1 = new User("Manuel", 34);
 // console.log(user1);
 // user1.greet();
+//-------------------------------------------
 
+// lesson 3
+// const hobbies = ["Sports", "Cooking", "Reading"];
+// console.log(hobbies[0]);
 
-const hobbies = ["Sports", "Cooking", "Reading"];
-console.log(hobbies[0]);
+// hobbies.push("Working");
+// console.log(hobbies);
 
-hobbies.push("Working");
-console.log(hobbies);
+// // const index = hobbies.findIndex((item) => {
+// //   return item === "Sports";
+// // });
+// const index = hobbies.findIndex((item) => item === "Sports"); //isto kako pogore, samo najkratka verzija
+// console.log(index);
 
-// const index = hobbies.findIndex((item) => {
-//   return item === "Sports";
-// });
-const index = hobbies.findIndex((item) => item === "Sports"); //isto kako pogore, samo najkratka verzija
-console.log(index);
+// const editedHobbies = hobbies.map((item) => item + "!");
+// const editedHobbies2 = hobbies.map((item) => ({ text: item }));
+// console.log(editedHobbies, editedHobbies2);
+//-------------------------------------------
 
-const editedHobbies = hobbies.map((item) => item + "!");
-const editedHobbies2 = hobbies.map((item) => ({ text: item }));
-console.log(editedHobbies, editedHobbies2);
+// lesson 4
+// / const userNameData = ["Max", "Schwarzmuller"];
+// // const fristName = userNameData[0];
+// // const lastName = userNameData[1];
+// const [fristName, lastName] = ["Max", "Schwarzmuller", 34];
+// console.log(fristName);
+// console.log(lastName);
+// // console.log(age);
+
+// const { name: userName, age } = {
+//   name: "Max",
+//   age: 34,
+// };
+// console.log(userName, age);
+// const name = user.name;
+// const age = user.age;
+//-------------------------------------------
