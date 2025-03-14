e40026e0562e54d3344f2ad19694551e69162caf
bazzomir <bazzomir97@gmail.com>
2025-03-14 04:18:01 +0100

    lesson 1

---
+++ react-guide-course-javascript-refresher/assets/script/app.js
@@ -0,0 +1,6 @@
+// import { export1, export2 as content2 } from "./util";
+// import export3 from "./util";
+// import * as until from "./util"; // kreira object od se
+
+// console.log(export1 + " and " + content2 + "..." + export3);
+// console.log(until);
---
+++ react-guide-course-javascript-refresher/assets/script/util.js
@@ -0,0 +1,10 @@
+// export let export1 = "This is export1";
+// export const export2 = "This is export2";
+
+// export default "This is export3";
+
+// // export function greating() {
+// //   console.log("Hello World!");
+// // }
+
+// // greating();
